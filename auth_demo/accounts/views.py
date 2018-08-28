f2fa700bf2c97f092a294d519fb4f024d8663eb7
Liam-Cafearo <lcafearo@gmail.com>
2018-08-28 19:25:28 +0100

    added stripe subscription code

--- auth_demo/accounts/views.py
+++ auth_demo/accounts/views.py
@@ -10,36 +10,60 @@ from django.template.context_processors import csrf
 from django.conf import settings
 import datetime
 import stripe
+import arrow
 
 
 # Create your views here.
 
 stripe.api_key = settings.STRIPE_SECRET
 
+
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             try:
-                customer = stripe.Charge.create(
-                    amount=999,
-                    currency="GBP",
-                    description=form.cleaned_data['email'],
+                # Stripe Single payment
+                # customer = stripe.Charge.create(
+                #     amount=999,
+                #     currency="GBP",
+                #     description=form.cleaned_data['email'],
+                #     card=form.cleaned_data['stripe_id'],
+                # )
+
+                # Stripe Subscription
+                customer = stripe.Customer.create(
+                    email=form.cleaned_data['email'],
+                    # this is currently the card token/id
                     card=form.cleaned_data['stripe_id'],
+                    plan='REG_MONTHLY',
                 )
-                if customer.paid:
-                    form.save()
-                    user = auth.authenticate(email=request.POST.get('email'),
-                                            password=request.POST.get('password1'))
+
+                # Stripe single Payment
+                # if customer.paid:
+                #     form.save()
+                #     user = auth.authenticate(email=request.POST.get('email'),
+                #                              password=request.POST.get('password1'))
+
+                # Stripe Subscription
+                if customer:
+                    user = form.save()
+                    user.stripe_id = customer.id
+                    user.subscription_end = arrow.now().replace(weeks=+4).datetime
+                    user.save()
 
                     if user:
                         auth.login(request, user)
-                        messages.success(request, "You have successfully registered")
+                        messages.success(
+                            request, "You have successfully registered")
                         return redirect(reverse('profile'))
 
                     else:
-                        messages.error(request, "unable to log you in at this time!")
-                else:messages.error(request, "We were unable to take a payment with that card!")
+                        messages.error(
+                            request, "unable to log you in at this time!")
+                else:
+                    messages.error(
+                        request, "We were unable to take a payment with that card!")
             except stripe.error.CardError, e:
                 messages.error(request, "Your card was declined!")
     else:
@@ -84,3 +108,13 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(reverse('index'))
+
+
+@login_required(login_url='/accounts/login/')
+def cancel_subscription(request):
+    try:
+        customer = stripe.Customer.retrieve(request.user.stripe_id)
+        customer.cancel_subscription(at_period_end=True)
+    except Exception, e:
+        messages.error(request, e)
+    return redirect('profile')
